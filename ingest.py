aae1b6cd4e6f6eeefbc0ce4f3099063ca1b75d2f
Arnav Agrawal <aa779@cornell.edu>
2026-01-19 10:26:08 -0800

    ingestion

--- ingest.py
+++ ingest.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 morphik = Morphik(uri=os.getenv("MORPHIK_URI"))
 
+result = morphik.ingest_directory(directory="files")
+
+for doc in result:
+    print(f"Ingested document {doc.filename} with id {doc.external_id}. Status: {doc.status['status']}")
